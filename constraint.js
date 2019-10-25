85e8048f59ef180dbf3e1b256f6adb821e485717
John Edwards <edwardsjohnmartin@gmail.com>
2019-10-25 10:13:05 -0700

    UI improvements

--- constraint.js
+++ constraint.js
@@ -31,12 +31,26 @@ DirConstraint.prototype.getDesc = function() {
   return this.dir + " of";
 }
 
-DirConstraint.prototype.test = function() {//srcGeometry, targetGeometry) {
+DirConstraint.prototype.srcPoint = function() {
+  let p = name2element[this.srcName].geometry;
+  return { x:p[0], y:p[1] };
+}
+
+DirConstraint.prototype.targetPoint = function() {
+  let p = name2element[this.targetName].geometry;
+  return { x:p[0], y:p[1] };
+}
+
+DirConstraint.prototype.midPoint = function() {
+  let p = this.srcPoint();
+  let q = this.targetPoint();
+  return { x:(p.x+q.x)/2, y:(p.y+q.y)/2 };
+}
+
+DirConstraint.prototype.test = function() {
   let srcPoint = name2element[this.srcName].geometry;
   let targetPoint = name2element[this.targetName].geometry;
-  // var srcPoint = srcGeometry;//.p;
-  // var targetPoint = targetGeometry;//.p;
-  // See if matches within 15 degrees
+  // See if matches within THRESHOLD degrees
   var v = subtract(srcPoint, targetPoint);
   var a = degrees(Math.atan2(v[1], v[0]));
   var diff = Math.abs(this.targetAngle - a);
--- main.js
+++ main.js
@@ -9,27 +9,27 @@ let flipy = d3.scaleLinear()
 
 let drag = function() {
 
-  function dragstarted(d) {
+  function dragstarted(elementName) {
     d3.select(this)
       .attr("stroke", "black");
   }
 
-  function dragged(d) {
+  function dragged(elementName) {
     let x = d3.event.x;
     let y = d3.event.y;
 
+    let d = elementName;
     name2element[d].geometry[0] = x;
     name2element[d].geometry[1] = flipy(y);
 
     // circle
     d3.select(this)
-      // .raise()
       .attr("cx", x)
       .attr("cy", y)
     ;
 
     // name
-    d3.select(`#${d}-text`)
+    d3.select(`text.element.label.${d}`)
       .attr("x", x)
       .attr("y", y)
     ;
@@ -47,6 +47,11 @@ let drag = function() {
       c.test();
       ce.style('stroke', c.sat ? 'black' : 'red');
     });
+
+    d3.selectAll(`.constraint.label.${elementName}`)
+      .attr('x', c => c.midPoint().x)
+      .attr('y', c => flipy(c.midPoint().y))
+    ;
   }
 
   function dragended(d) {
@@ -80,54 +85,75 @@ function init() {
     .attr('cy', (d,i) => flipy(name2element[d].geometry[1]))
     .attr('r', radius)
     .attr('id', d => d)
+    .attr('class', d => d.id)
     .style('fill', d => '#55ff55')
     .on('mouseover', function(d, i) {
       d3.select(this).attr('r', radius+2);
+      d3.selectAll(`.constraint.${d}`)
+        .attr('visibility', 'visible')
+      ;
     })
     .on('mouseout', function(d, i) {
       d3.select(this).attr('r', radius);
+      d3.selectAll(`line.constraint.${d}`)
+        .attr('visibility', c => c.sat ? 'hidden' : 'visible')
+      ;
+      d3.selectAll(`text.constraint.${d}`)
+        .attr('visibility', 'hidden')
+      ;
     })
     .call(drag())
   ;
 
   d3.select('svg')
-    .selectAll('text')
+    .selectAll('text.element')
     .data(data)
     .enter()
     .append('text')
     .text(d => d)
-    .attr('id', d => `${d}-text`)
+    .attr('class', d => `element label ${d}`)
+    // .attr('id', d => `${d}-text`)
     .attr('x', (d,i) => name2element[d].geometry[0])
     .attr('y', (d,i) => flipy(name2element[d].geometry[1]))
     .attr('text-anchor', "middle")
     .attr('alignment-baseline', 'middle')
   ;
 
-  let tooltip = new Tooltip();
+  // let tooltip = new Tooltip();
 
   // Constraints
-  svg.selectAll('line')
+  svg.selectAll('line.constraint')
     .data(constraints)
     .enter()
     .append('line')
-    .attr('x1', (d,i) => name2element[d.srcName].geometry[0])
-    .attr('y1', (d,i) => flipy(name2element[d.srcName].geometry[1]))
-    .attr('x2', (d,i) => name2element[d.targetName].geometry[0])
-    .attr('y2', (d,i) => flipy(name2element[d.targetName].geometry[1]))
-    .attr('id', d => d.id)
+    .attr('class', d => `constraint ${d.srcName} ${d.targetName}`)
+    .attr('x1', c => c.srcPoint().x)
+    .attr('y1', c => flipy(c.srcPoint().y))
+    .attr('x2', c => c.targetPoint().x)
+    .attr('y2', c => flipy(c.targetPoint().y))
+    .attr('id', c => c.id)
     .style('stroke', c => c.sat ? 'black' : 'red')
+    // .style('stroke-width', c => c.sat ? 0 : 2)
+    .style('stroke-width', 2)
     .lower()
-    .on("mouseover", d => {
-      tooltip.mouseover(d);
-    })
-    .on("mousemove", () => {
-      tooltip.mousemove();
-    })
-    .on("mouseout", () => {
-      tooltip.mouseout();
-    })
-    // .append("svg:title")
-    // .text(function(d, i) { return 'abc'; })
+  ;
+
+  d3.select('svg')
+    .selectAll('text.constraint')
+    .data(constraints)
+    .enter()
+    .append('text')
+    .text(c => `${c.srcName} ${c.getDesc()} ${c.targetName}`)
+    .attr('class', d => `constraint label ${d.srcName} ${d.targetName}`)
+    .attr('x', c => c.midPoint().x)
+    .attr('y', c => flipy(c.midPoint().y))
+    .attr('visibility', 'hidden')
+    .attr('text-anchor', "middle")
+    .attr('alignment-baseline', 'middle')
+  ;
+
+  d3.selectAll('line.constraint')
+    .attr('visibility', c => c.sat ? 'hidden' : 'visible')
   ;
 }
 
